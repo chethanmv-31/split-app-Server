673cfc28fdb849de5fc05a93707999fae87bed26
chethanmv-31 <chethan.makodu@gmail.com>
2026-02-12 14:43:36 +0530

    created profile page api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '15mb' }));
+  app.use(urlencoded({ extended: true, limit: '15mb' }));
   app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
